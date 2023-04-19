b3da68a05f7b58162d179106b00d78ec8f7953ae
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-04-19 15:01:20 -0700

    display text of ticket issue to the page in the big list of tickets.

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -5,7 +5,7 @@ function Ticket(props) {
   return (
     <React.Fragment>
       <h3>{props.location} - {props.names}</h3>
-      <p><em>props.issue</em></p>
+      <p><em>{props.issue}</em></p>
       <hr/>
     </React.Fragment>
   );
