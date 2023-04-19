f49cda2c9ba9d4c9b901cb863c270a112467f111
Zachary <ZackCipolletta@gmail.com>
2023-04-18 18:41:52 -0700

    add state and toggle between two components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import App from './components/App';
+import './index.css';
 
-
-function App(){
-  const name = "Thato";
-  const name2 = "Haley";
-  return (
-    <React.Fragment>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment. */}
-      <hr/>
-    </React.Fragment>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
